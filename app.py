30851424c907e36d2325434c3971869767fad5f2
myname21-meet <myname21@meet.mit.edu>
2019-07-30 14:14:40 +0300

    flask tough

--- app.py
+++ app.py
@@ -11,6 +11,16 @@ def display_student(student_id):
     return render_template('student.html', student=query_by_id(student_id))
 
 #Create an '/add' route here:
+@app.route('/add', methods = ['GET', 'POST'])
+def add_student_route():
+	if request.method == 'GET':
+		return render_template ('add.html')
+	else:
+		name = request.form['student_name']
+		year = request.form['student_year']
+		student1=add_student(name,year)
+		return render_template('add.html')\
 
+	
 if __name__ == '__main__':
     app.run(debug=True)
